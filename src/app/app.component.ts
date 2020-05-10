e55f69c62b538bb990040d0b32bd08159ca056c2
AnnaLaptop <anna.laskowska@hotmail.com>
2020-05-10 20:44:53 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import  {Task} from './task'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-project';
+  config: { [ key : string]: string |Date } = null;
+  tasks: Task[] = [
+    {
+    name: 'English learning one hour',
+    deadline: '2020-06-07',
+    done: false,
+    },
+    {
+      name: 'Dutch learning one hour',
+      deadline: '2020-06-08',
+      done: false,
+      },
+      {
+        name: 'French learning one hour',
+        deadline: '2020-06-09',
+        done: false,
+      }
+
+  ]
+  constructor() {
+    this.config = {
+      title: 'My tasks for next month',
+      footer: '@Lista zadan zbudowana w Angularze.',
+      date: new Date()
+    };
+  }
 }
